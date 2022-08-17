a01a365d3541b16a2b1bedfa7df026454bae2462
viry <vbatten10@hotmail.com>
2022-08-17 16:36:20 -0500

    Formatear array con map

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import useSelectMonedas from '../hooks/useSelectMonedas';
 import { monedas } from '../data/monedas';
@@ -22,7 +22,8 @@ const InputSubmit = styled.input`
 `;
 
 const Formulario = () => {
-    
+    const [criptos, setCriptos] = useState([]);
+
     const [moneda, SelectMonedas] = useSelectMonedas('Elige tu Moneda', monedas);
 
     useEffect(() => {
@@ -30,7 +31,15 @@ const Formulario = () => {
             const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
             const respuesta = await fetch(url);
             const resultado = await respuesta.json();
-            console.log(resultado.Data);
+
+            const arrayCriptos = resultado.Data.map(cripto => {
+                const objeto = {
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.FullName
+                }
+                return objeto;
+            });
+            setCriptos(arrayCriptos);
         }
         consultarAPI();
     }, []);
