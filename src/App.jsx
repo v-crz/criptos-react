6719003057bf96c074081302bfbb4b08509d3b99
viry <vbatten10@hotmail.com>
2022-08-16 19:43:57 -0500

    Input en formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styled from '@emotion/styled';
+import Formulario from './components/Formulario';
 import ImagenCripto from './img/imagen-criptos.png';
 
 const Contenedor = styled.div`
@@ -48,6 +49,7 @@ function App() {
       />
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario></Formulario>
       </div>
     </Contenedor>
   )
