30911a1c6d8b39c7305fac5d4f4a3588b9a0af9f
viry <vbatten10@hotmail.com>
2022-08-17 12:25:52 -0500

    Pasando opciones al hook

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import useSelectMonedas from '../hooks/useSelectMonedas';
+import { monedas } from '../data/monedas';
 
 const InputSubmit = styled.input`
     background-color: #9497FF;
@@ -12,6 +14,7 @@ const InputSubmit = styled.input`
     font-size: 20px;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
     &:hover {
         background-color: #7A7DFE;
         cursor: pointer;
@@ -19,14 +22,20 @@ const InputSubmit = styled.input`
 `;
 
 const Formulario = () => {
-  return (
+    
+    const [SelectMonedas] = useSelectMonedas('Elige tu Moneda', monedas);
+    // const [SelectCriptomonedas] = useSelectMonedas('Elige tu Criptomoneda');
+
+    return (
     <form>
+        <SelectMonedas/>
+        {/* <SelectCriptomonedas/> */}
         <InputSubmit
             type="submit" 
             value="Cotizar"
         />
     </form>
-  )
+    )
 }
 
 export default Formulario
